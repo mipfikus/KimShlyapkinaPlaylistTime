b8686d7ddaafe6366bea029b99a9d844346f0324
Mipfikus <info@mishkadesign.ru>
2024-11-06 12:36:23 +0300

    Создание необходимых функций

---
+++ main.py
@@ -0,0 +1,11 @@
+# Создание необходимых функций
+
+def get_duration():
+    # Код
+
+def select_random_songs():
+    # Код
+
+def calculate_duration():
+    # Вызов функций get_duration() и select_random_songs()
+    # Вывод ответа
